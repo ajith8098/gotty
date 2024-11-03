6d68c33b60f48e126b8d1e43e7d07f1ad7bd6f4d
ajith8098 <134246822+ajith8098@users.noreply.github.com>
2024-11-03 10:15:55 +0530

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Check if Gotty is installed, if not, install it
-if ! [ -x "$(command -v gotty)" ]; then
+if ! command -v gotty &> /dev/null; then
   echo 'Gotty is not installed. Installing...'
-  wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
-  tar -xvzf gotty_linux_amd64.tar.gz
+  wget -q https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
+  tar -xzf gotty_linux_amd64.tar.gz
   mv gotty /usr/local/bin/gotty
+  rm gotty_linux_amd64.tar.gz
+  echo 'Gotty installation complete.'
+else
+  echo 'Gotty is already installed.'
 fi
 
-# Start Gotty with unrestricted access
+# Start Gotty with unrestricted access on port 8080
+echo 'Starting Gotty...'
 /usr/local/bin/gotty --permit-write --reconnect --port 8080 /bin/bash
